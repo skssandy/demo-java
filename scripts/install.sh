a72129fa5949d232b3747ba7591b9a29637f9568
skssandy <89083444+skssandy@users.noreply.github.com>
2022-12-01 17:55:47 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/demo/
-sudo cp /var/www/demo/build/libs/*.war /opt/tomcat/apache-tomcat-8.0.53/webapps/
+cd /var/www/demo/build/libs
+sudo cp *.war /opt/tomcat/apache-tomcat-8.0.53/webapps/
