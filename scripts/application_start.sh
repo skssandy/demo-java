f9dbb58c41aeb2b7d1f47f9eeaf788d1f8b6ba37
skssandy <89083444+skssandy@users.noreply.github.com>
2023-05-26 16:50:38 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/demo/build/libs
-sudo cp demo-0.0.1-SNAPSHOT.war /opt/tomcat/apache-tomcat-8.0.53/webapps/
+sudo cp demo-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/
 sudo systemctl restart tomcat.service
